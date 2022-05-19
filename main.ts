11a43ffc76a2335f513ce3fe78f4c2658726fe86
Thea2008 <103181295+Thea2008@users.noreply.github.com>
2022-05-19 11:27:33 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.Y) < -200) {
+        if (input.acceleration(Dimension.X) < 0) {
+        	
+        }
+    } else {
+    	
+    }
 })
